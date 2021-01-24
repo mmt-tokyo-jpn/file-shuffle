d710c6841ffbb900a688fedf61c08bb9ca66a90a
mmt-tokyo-japan <mmt.tokyo.jpn@gmail.com>
2021-01-24 19:04:03 +0900

    Added another test

--- src/file_shuffule_try/test/test_file_shuffule_try.py
+++ src/file_shuffule_try/test/test_file_shuffule_try.py
@@ -3,6 +3,9 @@ from typing import Iterable
 from math import log10
 import pytest
 import re
+import os
+import shutil
+import glob
 
 
 def remove_prefix(input_str: str) -> str:
@@ -44,15 +47,13 @@ def test_shuffle_10_files() -> None:
 
 
 def test_shuffle_10_files_reshuffle() -> None:
-    files = [str(i+1).zfill(2) + "-" + "name" + str(i + 1) for i in range(10)]
+    files = [str(i + 1).zfill(2) + "-" + "name" + str(i + 1) for i in range(10)]
 
     numbers = range(1, len(files) + 1)
     result_file_names = list(create_file_names(files, numbers))
 
     assert "01-name1" in result_file_names
     assert "03-name3" in result_file_names
-
-
 
 
 remove_prefix_test_data = [
@@ -68,3 +69,35 @@ def test_remove_prefix_parameterized(input_str: str, expected: str) -> None:
     result = remove_prefix(input_str)
 
     assert result == expected
+
+
+def test_add_prefix_to_the_real_file():
+    test_folder = "test_folder"
+    assert not os.path.exists(test_folder)
+
+    folder_to_return = os.getcwd()
+    test_folder_path = os.path.join(folder_to_return, test_folder)
+
+    try:
+        os.mkdir(test_folder_path)
+        os.chdir(test_folder_path)
+        print(os.getcwd())
+
+        open("test_file1.txt", "a").close()
+
+        result_filenames = create_file_names(["test_file1.txt"], [1])
+
+        os.rename("test_file1.txt", list(result_filenames)[0])
+
+        files_in_folder = list(glob.glob("./*"))
+
+        assert any("1-test_file1.txt" in s for s in files_in_folder)
+
+        for n in files_in_folder:
+            print(n)
+
+
+    finally:
+        print(folder_to_return)
+        os.chdir(folder_to_return)
+        shutil.rmtree(test_folder_path, ignore_errors=True)
