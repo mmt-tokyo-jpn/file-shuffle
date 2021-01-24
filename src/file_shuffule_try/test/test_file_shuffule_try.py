b73b8a7930137f449996cda3bd70794d11e881b1
Toshi Sakura <toshi.sakura@gmail.com>
2021-01-24 12:42:09 +0900

    Passed test_shuffle_10_files

--- src/file_shuffule_try/test/test_file_shuffule_try.py
+++ src/file_shuffule_try/test/test_file_shuffule_try.py
@@ -1,5 +1,6 @@
 from file_shuffule_try.file_shuffule_try import fib
 from typing import Iterable
+from math import log10
 
 def test_fib() -> None:
     assert fib(0) == 0
@@ -14,10 +15,13 @@ def test_fib() -> None:
 
 def create_file_names(files: Iterable[str], numbers: Iterable[int]) -> Iterable[str]:
 
-    inputs = zip(files, numbers)
+    inputs = list(zip(files, numbers))
+
+    numInputs = len(inputs)
+    numLetters = int(log10(numInputs) + 1)
 
     for file, number in inputs:
-        yield str(number) + "-" + file
+        yield str(number).zfill(numLetters) + "-" + file
 
 
 
@@ -28,9 +32,21 @@ def test_shuffle_one_file() -> None:
     assert list(result_file_names) == ["1-name"]
 
 
-def test_shuffle_one_file2() -> None:
+def test_shuffle_two_files() -> None:
 
     result_file_names = create_file_names(["name1", "name2"], [1, 2])
 
     assert set(result_file_names) == {"1-name1", "2-name2"}
+
+
+def test_shuffle_10_files() -> None:
+
+    files = ["name" + str(i+1) for i in range(10)]
+
+    numbers = range(1, len(files)+1)
+    result_file_names = create_file_names(files, numbers)
+
+    assert "01-name1" in result_file_names
+    assert "03-name3" in result_file_names
+
 
